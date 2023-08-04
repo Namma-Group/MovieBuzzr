fbf7b9117f061e2644fac515079c8f7a33c51949
gowtham20402 <gowthamchandrasekaran01@gmail.com>
2023-08-04 14:09:19 +0530

    chumma

--- src/main/java/dev/nammaGroup/moviesbuzzr/MoviesbuzzrApplication.java
+++ src/main/java/dev/nammaGroup/moviesbuzzr/MoviesbuzzrApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MoviesbuzzrApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesbuzzrApplication.class, args);
 	}
