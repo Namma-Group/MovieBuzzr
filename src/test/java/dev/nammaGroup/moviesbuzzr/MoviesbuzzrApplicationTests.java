d2e3ebd01b76ceb319c1377bb5791468301351eb
saravana4yu <saravanananthaa@gmail.com>
2023-08-04 12:47:41 +0530

    Project Files

---
+++ src/test/java/dev/nammaGroup/moviesbuzzr/MoviesbuzzrApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nammaGroup.moviesbuzzr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesbuzzrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
