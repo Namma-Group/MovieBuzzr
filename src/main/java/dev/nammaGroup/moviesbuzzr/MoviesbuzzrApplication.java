18c048652fbd5e715b73486e8cf0cda7a45f4055
Saravana Kumar N <saravanananthaa@gmail.com>
2023-08-04 16:00:25 +0530

    Update MoviesbuzzrApplication.java
    Removed the unused method

--- src/main/java/dev/nammaGroup/moviesbuzzr/MoviesbuzzrApplication.java
+++ src/main/java/dev/nammaGroup/moviesbuzzr/MoviesbuzzrApplication.java
@@ -2,9 +2,6 @@ package dev.nammaGroup.moviesbuzzr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MoviesbuzzrApplication {
@@ -12,20 +9,6 @@ public class MoviesbuzzrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesbuzzrApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("*")
-						.allowedMethods("*")
-						.allowedHeaders("*")
-						.allowCredentials(false).maxAge(3600);
-			}
-		};
 	}
 
 }
